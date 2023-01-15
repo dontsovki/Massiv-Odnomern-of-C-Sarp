8e8d2c0a5fc663a713a492a3601a8350c347b9c4
Artem.don <dontsovki@gmail.com>
2023-01-15 16:16:22 +0300

    Программа заполнения массива через функцию, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.

---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Программа заполнения массива через функцию, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+int[] array = GetBinaryArray(8);
+Console.WriteLine($" [ {String.Join(" , ", array)} ] ");
+
+
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(2);
+    }
+
+    return result;
+}
+
+
+
+
+//Console.WriteLine("Hello, World!");
+//int [] boolNum = new int [8];
+
+//for (int i=0; i < 8; i++){
+//    boolNum[i] = new Random().Next(0, 2);
+//    Console.Write(boolNum[i]);
+//}
+

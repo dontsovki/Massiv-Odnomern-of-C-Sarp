1a9411bb2b0f51a78718d2c01ec28b9200ae83db
Artem.don <dontsovki@gmail.com>
2023-01-25 19:13:05 +0300

    программа через char[] которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа

---
+++ Task6/Program.cs
@@ -0,0 +1,7 @@
+﻿// программа через char[] которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+Console.Write("введите число ");
+string str = Console.ReadLine();
+   Console.WriteLine("Вы ввели не трехзначное число");
+char[] a=str.ToCharArray();
+
+Console.Write($"Второе число {a[1]} из числа {str}");

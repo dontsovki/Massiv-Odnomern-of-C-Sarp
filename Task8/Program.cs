125f679daff308e1645694fc758417cc94d8982a
Artem.don <dontsovki@gmail.com>
2023-02-25 15:47:53 +0300

    Функции перевода массива чисел в строку

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,16 +1,39 @@
-﻿//функция преобразования числового массива в строку
+﻿// Функции перевода массива чисел в строку
+// функция преобразования числового массива в строку
+// int[] bases = { 2, 8, 10, 16};
+// int[] numbers = { Int32.MinValue, -19327543, -13621, -18, 12,
+//                              19142, Int32.MaxValue };
 
-int main() // функция преобразования числового массива в строку
+// foreach (int baseValue in bases)
+// {
+//    Console.WriteLine("Base {0} conversion:", baseValue);
+//    foreach (int number in numbers)
+//    {
+//       Console.WriteLine("   {0,-15}  -->  0x{1}",
+//                         number, Convert.ToString(number, baseValue));
+//    }
+// }
+
+// public class Example  // Метод перевода массива в строку использование имеет конструктор, который принимает массив символов в качестве параметра и инициализирует новый экземпляр класса String соответствующими символами. В следующем примере создается экземпляр строки из массива символов
+// {
+//     public static void Main()
+//     {
+//         char[] chars = {'s', 'o', 'm', 'e', ' ', 's', 't', 'r', 'i', 'n', 'g'};
+ 
+//         string s = new string(chars);
+ 
+//         Console.WriteLine(s);        // какая-то строка
+//     }
+// }
+
+public class Example  // Метод перевода массива в строку использование String.Join метод для объединения элементов массива с разделителем
 {
-int size = 15;
-int digits[size] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 0, 4};
-std::string result = "";
-
-for (int i = 0; i < size; i++)
-{
-if (!(digits[i] == 0 && result.size() == 0))
-result += std::to_string(digits[i]);
-}
-
-std::cout << result << std::endl;
+    public static void Main()
+    {
+        char[] chars = {'s', 'o', 'm', 'e', ' ', 's', 't', 'r', 'i', 'n', 'g'};
+ 
+        string s = string.Join(string.Empty, chars);
+ 
+        Console.WriteLine(s);        // какая-то строка
+    }
 }

07d903b6cdcdcfc589720b81203fc26534673365
Artem.don <dontsovki@gmail.com>
2023-02-25 13:33:55 +0300

    функция преобразования числового массива в строку

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿//функция преобразования числового массива в строку
+
+int main() // функция преобразования числового массива в строку
+{
+int size = 15;
+int digits[size] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 0, 4};
+std::string result = "";
+
+for (int i = 0; i < size; i++)
+{
+if (!(digits[i] == 0 && result.size() == 0))
+result += std::to_string(digits[i]);
+}
+
+std::cout << result << std::endl;
+}
